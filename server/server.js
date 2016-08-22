bffbdb0525e8457f67877d49c5074fa3e38ba1e2
Luke <lukeaveil@yahoo.com>
2016-08-22 12:38:50 +0100

    sets up webpack and initial react component

--- server/server.js
+++ server/server.js
@@ -3,13 +3,15 @@ var path = require('path');
 
 var app = express();
 
-app.use('/', function (req, res){
+app.use(express.static('./dist'));
+
+app.use('/', function (req, res) {
   res.sendFile(path.resolve('client/index.html'));
 });
 
 var port = 3000;
 
-app.listen(port, function(error){
+app.listen(port, function(error) {
   if (error) throw error;
   console.log('Express server listening on port', port);
 });
