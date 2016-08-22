0c9f3db5358601700fc447d1c9b900c747930adc
Luke <lukeaveil@yahoo.com>
2016-08-22 10:16:47 +0100

    sets up server for react tutorial

---
+++ server/server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+
+app.use('/', function (req, res){
+  res.sendFile(path.resolve('client/index.html'));
+});
+
+var port = 3000;
+
+app.listen(port, function(error){
+  if (error) throw error;
+  console.log('Express server listening on port', port);
+});
