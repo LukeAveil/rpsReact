c7a22b9f2df6ab4425908d060ac245cd8cd58319
Luke <lukeaveil@yahoo.com>
2016-08-25 09:51:05 +0100

    updates components

--- components/TextInput.js
+++ components/TextInput.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+ import React, { Component } from 'react'
 import TextDisplay from './TextDisplay'
 
 class TextInput extends Component {
@@ -8,6 +8,12 @@ class TextInput extends Component {
     this.state = {
       inputText: 'initial text'
     }
+  }
+
+  deleteLetter() {
+    this.setState({
+      inputText: this.state.inputText.substring(0, this.state.inputText.length - 1)
+    })
   }
 
   handleChange(event) {
@@ -24,7 +30,7 @@ class TextInput extends Component {
           value={this.state.inputText}
           onChange={this.handleChange.bind(this)}
         />
-        <TextDisplay text={this.state.inputText}/>
+        <TextDisplay text={this.state.inputText} deleteLetter={this.deleteLetter.bind(this)}/>
       </div>
     )
   }
